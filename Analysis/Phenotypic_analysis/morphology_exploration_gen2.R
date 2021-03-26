b2ba17beed6c4d58e9d60ffa104c52f0f934628c
BenjaminATaylor <benjamin.aaron.taylor@gmail.com>
2021-03-26 16:25:09 +0000

    Data cleaning

--- Analysis/Phenotypic_analysis/morphology_exploration_gen2.R
+++ Analysis/Phenotypic_analysis/morphology_exploration_gen2.R
@@ -18,30 +18,56 @@ measure.vars = c("Days_germ", #days from potting to germination
                  "Num_leaves", #number of leaves
                  "Sla", #single leaf area of oldest non-cotyledon
                  "Sldw", #single lead dry weight of oldest non-cotyledon
-                 "Sp_la", #
+                 #"Sp_la", #specific leaf area
                  "Aboveground_dw", #all biomass above soil
-                 "Root_dw", #all biomass below soil
+                 "Root_dw" #all biomass below soil
                  #"Total_biomass", #sum of above and below ground biomass
-                 "Day_flowering", #days from germination to flowering
-                 "Root_to_shoot_ratio") #
-
+                 #"Day_flowering", #days from germination to flowering
+                 #"Root_to_shoot_ratio" #ratio of below to above-ground biomass (I think)
+                 ) 
 
 # convert to numeric where applicable
-mutate_at(phenodata.gen2, c(Day_flowering,
-                            Num_leaves,
-                            Height,
-                            sla,
-                            sldw,
-                            leaf_length,
-                            ), )
+phenodata.gen2.clean =  mutate_at(phenodata.gen2, measure.vars, function(x) as.numeric(as.character(x))) %>%
+  select(c("Species","Population","Wild_Dom","Environment",measure.vars)) 
+
+#the number of complete cases is very small...
+dim(phenodata.gen2.clean)
+table(complete.cases(phenodata.gen2.clean))
+#...but let's see how things look using just complete cases anyway
+phenodata.gen2.clean.compcases = phenodata.gen2.clean[complete.cases(phenodata.gen2.clean),]
+
+##pca for brassicas
+# scale measure vars
+pca.counts = phenodata.gen2.clean.compcases %>% 
+  subset(substr(Species,1,8)=="Brassica") %>%
+  select(measure.vars) %>%
+  scale()
+#create pca object
+data.pca = prcomp(pca.counts)
+#extract PC data
+percent.var = (data.pca$sdev^2 / sum(data.pca$sdev^2))
+pca.out = list(values = data.frame(data.pca$x),
+               percent.var = percent.var)
+#connect to phenotypic data
+ggpcadata = pca.out$values %>% 
+  rownames_to_column(var = "WaspID") %>%
+  left_join(exp1.phenodata.clean,
+            by = "WaspID")
+#plot
+ggplot(ggpcadata, aes(x = PC1, y = PC2, color = Treatment, label = WaspID)) +
+  geom_point(size = 5, position = position_jitter(width = 0.5,height=0.5)) +
+  geom_text(vjust = -1) +
+  xlab(paste0("PC",1,": ",signif(pca.out$percent.var[1]*100, 3),"%")) +
+  ylab(paste0("PC",2,": ",signif(pca.out$percent.var[2]*100, 3),"%")) +
+  theme_bw() +
+  # scale_color_manual(name = "Treatment",
+  #                    values = brewer.pal(7, "Paired")) +
+  scale_shape_manual(name = "Treatment",
+                     values = c(8,15:20)) +
+  theme(panel.grid = element_line(color = "grey95"),
+        legend.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11),
+        axis.text.y = element_text(size = 11),
+        axis.title = element_text(face = "bold", size =12))
 
 
-
-## change class of some columns
-data$Species<-as.character(data$Species)
-data$Day_flowering<-as.numeric(as.character(data$Day_flowering))
-data$Num_leaves<-as.numeric(as.character(data$Num_leaves))
-data$Height<-as.numeric(as.character(data$Height))
-data$sla<-as.numeric(as.character(data$sla))
-data$sldw<-as.numeric(as.character(data$sldw))
-data$leaf_length<-as.numeric(as.character(data$leaf_length))
