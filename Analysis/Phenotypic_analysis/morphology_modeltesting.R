1a0038e7e5cc9dd968274a88c0a9550b67863d43
BenjaminATaylor <benjamin.aaron.taylor@gmail.com>
2021-05-25 21:49:56 +0100

    Generate tables and figures for phenotypic data exploration

--- Analysis/Phenotypic_analysis/morphology_modeltesting.R
+++ Analysis/Phenotypic_analysis/morphology_modeltesting.R
@@ -1,10 +1,12 @@
 library(rcompanion)
 
 phenodata.gen2.clean.brass.trans = phenodata.gen2.clean.brass %>%
-  mutate(Days_germ = transformTukey(Days_germ))
+  mutate(Root_dw = transformTukey(Root_dw))
 
-response_var = measure.vars[1]
+response_var = measure.vars[8]
 response_var
+ggqqplot(phenodata.gen2.clean.brass.trans$Root_dw)
+
 foo = lmer(scale(get(response_var))~Environment + Wild_Dom + Environment:Wild_Dom + (1|Wild_Dom/Population), 
            data=phenodata.gen2.clean.brass.trans, 
            control = lmerControl(optimizer="bobyqa", optCtrl = list(maxfun = 10000)))
@@ -13,4 +15,7 @@ check_model(foo)
 
 
 
-glmer(Aboveground_dw~Environment + Wild_Dom + Environment:Wild_Dom + (1|Wild_Dom/Population), data=brp_stand_off,family="Gamma",glmerControl(optimizer="bobyqa", optCtrl = list(maxfun = 10000)))
+goo = glmer(get(response_var)~Environment + Wild_Dom + Environment:Wild_Dom + (1|Wild_Dom/Population),
+      data=phenodata.gen2.clean.brass,family="Gamma",glmerControl(optimizer="bobyqa", optCtrl = list(maxfun = 10000)))
+
+summary(goo)
