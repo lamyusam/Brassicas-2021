9e20fb21ac428a73a785f0ea843f30a72499808a
BenjaminATaylor <benjamin.aaron.taylor@gmail.com>
2021-06-22 12:11:11 +0100

    Bash files for RBH blast

--- Analysis/Orthology/BLAST_Brap_vs_Arabid.sh
+++ Analysis/Orthology/BLAST_Brap_vs_Arabid.sh
@@ -14,20 +14,21 @@
 #SBATCH --ntasks-per-node=8
 
 # Set the name of the task.
-#SBATCH --job-name=BLAST_raph_vs_arabid
+#SBATCH --job-name=BLAST_brap_vs_arabid
 
 # Set working directory
-#SBATCH --chdir=/home/bat1m21/scratch/brassica-project/analysis/Raphanus/RBH_BLAST
+#SBATCH --chdir=/home/bat1m21/scratch/brassica-project/analysis/Brassica/RBH_BLAST
 
 #load BLAST
 module load blast/2.11.0
 
 # create BLAST databases
 makeblastdb -in species_fastas/arabidopsis.faa -dbtype 'prot' -out databases/arabidopsis_db
-makeblastdb -in species_fastas/raph.faa -dbtype 'prot' -out databases/raphanus_db
+makeblastdb -in species_fastas/brapa.faa -dbtype 'prot' -out databases/brassica_db
 
 # run BLAST search
 # -evalue: equivalent to significance value cutoff (lower values = more stringent matching)
 # -outfmt: output format (6 = tabular)
-blastp -num_threads 8 -evalue 1 -outfmt 6 -query species_fastas/arabidopsis.faa -db databases/raphanus_db > arabid_to_raph
-blastp -num_threads 8 -evalue 1 -outfmt 6 -query species_fastas/raph.faa -db databases/arabidopsis_db > raph_to_arabid
+blastp -num_threads 8 -evalue 1 -outfmt 6 -query species_fastas/arabidopsis.faa -db databases/brassica_db > arabid_to_brapa
+blastp -num_threads 8 -evalue 1 -outfmt 6 -query species_fastas/brapa.faa -db databases/arabidopsis_db > brapa_to_arabid
+
