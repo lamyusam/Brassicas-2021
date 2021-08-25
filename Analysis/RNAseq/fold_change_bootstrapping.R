99a7404783d4bdb2e8464d3418b524c133e5bc94
BenjaminATaylor <benjamin.aaron.taylor@gmail.com>
2021-08-25 20:08:17 +0100

    Rework LFC analysis to include interactions for Brapa plus subsetted otherwilds

--- Analysis/RNAseq/fold_change_bootstrapping.R
+++ Analysis/RNAseq/fold_change_bootstrapping.R
@@ -11,7 +11,7 @@ allwilds = c("Brassica rapa",otherwilds)
 #NB this excludes Rupestris, because that species has only two replicates
 
 #pick number of bootstraps to run (keep low for now)
-boot = 20
+boot = 2
 for(sp in 1:length(allwilds)){
   #narrow down to data for focal wild
   focal.wild = as.character(allwilds[sp])
@@ -34,21 +34,55 @@ for(sp in 1:length(allwilds)){
     degs.focalwild = results(dds.gene.deg.focalwild,
                              name="treatment_Control_vs_Wheat",
                              alpha = 0.05)
+    
+    #get number of degs
+    nDEGs = nrow(subset(degs.focalwild,padj<0.05))
+    
     #save results of loop
-    if(i==1){degsframe.focalwild = data.frame(degs.focalwild$log2FoldChange)}else{
-      degsframe.focalwild = cbind(degsframe.focalwild,degs.focalwild$log2FoldChange)}
+    if(i==1){focalwild.nDEGS = c(nDEGs)}else{
+      focalwild.nDEGS = c(focalwild.nDEGS,nDEGs)}
   }
   
   #save data for species
-  focal.lfcs = rowMeans(degsframe.focalwild)
-  if(sp == 1){lfcs.frame = data.frame(focal.lfcs)}else{
-    lfcs.frame = cbind(lfcs.frame, focal.lfcs)
+  if(sp == 1){degs.frame = data.frame(focalwild.nDEGS)}else{
+    degs.frame = cbind(degs.frame, focalwild.nDEGS)
   }
 }
 #give colnames to output frame
-colnames(lfcs.frame) = allwilds
-rownames(lfcs.frame) = row.names(degs.focalwild)
+colnames(degs.frame) = allwilds
 beepr::beep(3)
+#now this is really weird... per this, the group that's most plastic by far is Brassica macrocarpa!?
+
+#let's try another way and see if the same result comes out: run DESeq2 interaction models for each species vs B rapa
+
+for(i in 1:length(otherwilds)){
+  focal = otherwilds[i]
+  metadata.brass.focalandrapa = subset(metadata.brass.wilds, species %in% c(focal,"Brassica rapa"))
+  brass.gene.counts.clean.focalandrapa = brass.gene.counts.clean[,as.character(metadata.brass.focalandrapa$sample)]
+  #remove spaces so that DESeq doesn't complain
+  metadata.brass.focalandrapa$species = str_replace(metadata.brass.focalandrapa$species,pattern = " ",replacement = ".")
+  table(colnames(brass.gene.counts.clean.focalandrapa) == metadata.brass.focalandrapa$sample)
+  
+  #get DESeq2 output for focal wild
+  dds.gene.focalandrapa = DESeqDataSetFromMatrix(countData = brass.gene.counts.clean.focalandrapa,
+                                                 colData = metadata.brass.focalandrapa,
+                                                 design = as.formula(~treatment+species+treatment*species))
+  dds.gene.deg.focalandrapa = DESeq(dds.gene.focalandrapa, fitType = "parametric", betaPrior = FALSE)
+  degs.focalandrapa = results(dds.gene.deg.focalandrapa,
+                              name="treatmentControl.speciesBrassica.rapa",
+                              alpha = 0.05)
+  nUp = nrow(subset(degs.focalandrapa,padj<0.05 & log2FoldChange<0))
+  nDown = nrow(subset(degs.focalandrapa,padj<0.05 & log2FoldChange>0))
+  if(i==1){interDEGs = data.frame(c(nUp,nDown))}else{
+    interDEGs = cbind(interDEGs, data.frame(c(nUp,nDown)))}
+}
+colnames(interDEGs) = otherwilds
+
+
+
+
+
+
 
 #melt for purposes of anova
 testframe = rownames_to_column(lfcs.frame) %>% reshape2::melt()
